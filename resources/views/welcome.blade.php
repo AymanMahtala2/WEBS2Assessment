ddd2a07dca6180c4e841eec857c640419ffe8406
AymanMahtala2 <38426439+AymanMahtala2@users.noreply.github.com>
2019-05-15 18:53:48 +0200

    app.layouts gemaakt

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,41 +1,11 @@
-<html>
-<head>
+@extends('layouts.app')
 
-    <title>CentjeAyman</title>
-
-    <link href="{{ asset('css/CentjeOverview.css') }}" rel="stylesheet">
-    <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800'
-          rel='stylesheet' type='text/css'>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-</head>
-
-<body>
-<header class="masthead clear">
-  <div class="centered">
-
-    <div class="site-branding">
-      <h1 class="site-title">Centje Overview</h1>
-    </div>
-  </div>
-</header>
-
-<div class="sidenav">
-  <a href="#">Overzicht</a>
-  <a href="#">Stuur betaalverzoek</a>
-  <a href="#">Groepen</a>
-  <a href="#">Klagen</a>
-</div>
-
-<main class="main-area">
-
-  <div class="centered">
+@section('content')
+<div class="centered">
   <p>Betalingenoverzicht: groen = uitgaand (dus krijg je), rood = inkomend (moet je geven), grijs is inactief</p>
     <section class="cards">
-
+    
     </section>
 
   </div>
-
-</main>
-</body>
-</html>
+  @endsection
