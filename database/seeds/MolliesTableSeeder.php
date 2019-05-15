a78bd176a0c7bd3411bafa65db04f2d712c16c21
AymanMahtala2 <38426439+AymanMahtala2@users.noreply.github.com>
2019-05-15 20:01:42 +0200

    seeders aangemaakt (leeg)

---
+++ database/seeds/MolliesTableSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class MolliesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
